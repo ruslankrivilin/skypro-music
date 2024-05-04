c904c459c9fb34feccfadaf19351ef9f8e9b1cec
ruslankrivilin <ruslankrivilin@gmail.com>
2024-05-04 13:12:38 +0300

    hwTest

--- webdev-react-hw-template-main/skypro-music/src/components/Filters/FilterItem/FilterItem.tsx
+++ webdev-react-hw-template-main/skypro-music/src/components/Filters/FilterItem/FilterItem.tsx
@@ -8,7 +8,9 @@ import { FilterItemType } from "@/Types";
 export default function FilterItem({ handleFilterClick, title, list, isOpened }: FilterItemType) {
     return (
         <>
-            <div onClick={() => handleFilterClick(title)} className={classNames(styles.filterButton, styles.BtnText, {
+            <div 
+            onClick={() => handleFilterClick(title)} 
+            className={classNames(styles.filterButton, styles.BtnText, {
                 [styles.active]: isOpened,
             })}>
                 {title}
@@ -26,3 +28,34 @@ export default function FilterItem({ handleFilterClick, title, list, isOpened }:
         </>
     )
 }
+
+
+
+{/* <>
+{isOpened ? (
+    <div>
+        <div 
+            onClick={() => handleFilterClick(title)} 
+            className={classNames(styles.filterButton, styles.BtnText)}
+            >
+                {title}
+        </div>
+        <div  className={styles.filterButtonSpisok}>
+                    <ul className={styles.listBox}>
+                        {list.map((item) => (
+                            <li  className={styles.BtnSpisok} key={item}>{item}</li>
+                        ))}
+                    </ul>
+        </div>
+    </div>
+) : (
+    <div 
+            onClick={() => handleFilterClick(title)} 
+            className={classNames(styles.filterButton, styles.BtnText)}
+            >
+                {title}
+        </div>
+)}
+</>     
+)  ;     
+}         */}
