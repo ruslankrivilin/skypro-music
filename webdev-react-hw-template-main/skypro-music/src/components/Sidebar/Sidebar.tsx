9eb81b954d68d79ed38a19376104a6cd391e50fc
ruslankrivilin <ruslankrivilin@gmail.com>
2024-06-12 00:34:49 +0300

    hw4.4

--- webdev-react-hw-template-main/skypro-music/src/components/Sidebar/Sidebar.tsx
+++ webdev-react-hw-template-main/skypro-music/src/components/Sidebar/Sidebar.tsx
@@ -4,14 +4,6 @@ import styles from "./Sidebar.module.css";
 export default function Sidebar() {
     return (
         <div className={styles.mainSidebar}>
-          <div className={styles.sidebarPersonal}>
-            <p className={styles.sidebarPersonalName}>Sergey.Ivanov</p>
-            <div className={styles.sidebarIcon}>
-              <svg>
-                <use xlinkHref="img/icon/sprite.svg#logout" />
-              </svg>
-            </div>
-          </div>
           <div className={styles.sidebarBlock}>
             <div className={styles.sidebarList}>
               <div className={styles.sidebarItem}>
